9c89a5e269d4317066d3138a958dfa562d477848
Aditi Jain <aditijain7836@gmail.com>
2025-05-02 15:38:23 +0530

    Created top doctors component

--- src/components/SpecialityMenu.jsx
+++ src/components/SpecialityMenu.jsx
@@ -9,7 +9,7 @@ const SpecialityMenu = () => {
         <p className='sm:w-1/3 text-center text-sm'>
             Simply browse through our extensive list of trusted doctors, schedule your appointment hassle-free.
         </p>
-        <div className='flex sm:justify-center gap-4 pt-5 w-full overflow-scroll'>
+        <div className='flex flex-wrap sm:justify-center gap-4 pt-5 w-full overflow-scroll'>
             {specialityData.map((item, index) => (
                 <Link key={index} to={`/doctors/${item.speciality}`} className='flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-10px] transition-all duration-500' >
                     <img className='w-15 sm:w-24' src={item.image} alt=""/>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
     extend: {
       colors:{
         'primary': "#5f6FFF"
+      },
+      gridTemplateColumns:{
+        'auto': 'repeat(auto-fill, minmax(200px, 1fr))'
       }
     },
   },
